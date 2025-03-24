09c986135abdbeb03974ad292d0afef1d8fb860d
Johan Alexis Duque Cadena <johan.duque@gmail.com>
2025-03-24 22:41:31 +0100

    First draft of library

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use jsonwebtoken::crypto::verify;
 use jsonwebtoken::jwk::{AlgorithmParameters, Jwk, JwkSet};
-use jsonwebtoken::{DecodingKey, TokenData};
+use jsonwebtoken::{Algorithm, DecodingKey, Header, TokenData, Validation, decode, decode_header};
 use serde::{Deserialize, Serialize};
 use serde_json::{self};
 
-pub enum Error {
+pub enum ErrorInJwt {
     InvalidJwt,
     InvalidJwks,
     InvalidJwk,
@@ -15,6 +15,9 @@ pub enum Error {
     NoIssuer,
     NoSub,
     NoJwkForKid,
+    NotPossibleToGetDecodeKey,
+    ErrorVerifying,
+    NoSignaturePresent,
 }
 
 pub enum JwksEnum {
@@ -25,11 +28,11 @@ pub enum JwksEnum {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
-    aud: String,
-    sub: String,
-    company: String,
-    exp: u64,
-    iss: String,
+    pub aud: String,
+    pub company: String,
+    pub sub: String,
+    pub exp: u64,
+    pub iss: String,
 }
 
 #[derive(Debug, Serialize)]
@@ -38,21 +41,21 @@ struct SerializableTokenData<'a> {
     claims: &'a Claims,
 }
 
-fn get_public_key(kid: &str, jwk: &Jwk) -> Result<DecodingKey, Error> {
+fn get_public_key(jwk: &Jwk) -> Result<DecodingKey, ErrorInJwt> {
     // Search the kid in the jwks
-    let res: Result<DecodingKey, Error>;
+    let res: Result<DecodingKey, ErrorInJwt>;
 
     res = match &jwk.algorithm {
         AlgorithmParameters::RSA(rsa_params) => {
             Ok(DecodingKey::from_rsa_components(&rsa_params.n, &rsa_params.e).unwrap())
         }
 
-        _ => Err(Error::InvalidJwks),
+        _ => Err(ErrorInJwt::NotPossibleToGetDecodeKey),
     };
     res
 }
 
-pub fn check_token(token: &TokenData<Claims>) -> Result<String, Error> {
+pub fn check_token(token: &TokenData<Claims>) -> Result<String, ErrorInJwt> {
     let serializable = SerializableTokenData {
         header: &token.header,
         claims: &token.claims,
@@ -60,44 +63,44 @@ pub fn check_token(token: &TokenData<Claims>) -> Result<String, Error> {
 
     match serde_json::to_string(&serializable) {
         Ok(json) => Ok(json),
-        Err(_) => Err(Error::InvalidJson),
+        Err(_) => Err(ErrorInJwt::InvalidJson),
     }
 }
 
-pub fn get_kid_from_token(token: &TokenData<Claims>) -> Result<String, Error> {
-    match token.header.kid.as_ref() {
+pub fn get_kid_from_token(the_header: &Header) -> Result<String, ErrorInJwt> {
+    match the_header.kid.clone() {
         Some(kid) if !kid.is_empty() => Ok(kid.clone()),
-        Some(_) => Err(Error::InvalidToken),
-        None => Err(Error::InvalidToken),
+        Some(_) => Err(ErrorInJwt::InvalidToken),
+        None => Err(ErrorInJwt::InvalidToken),
     }
 }
 
-pub fn has_token_expired(token: &TokenData<Claims>) -> Result<(), Error> {
+pub fn has_token_expired(token: &TokenData<Claims>) -> Result<(), ErrorInJwt> {
     let current_time = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
-        .map_err(|_| Error::InvalidToken)?
+        .map_err(|_| ErrorInJwt::InvalidToken)?
         .as_secs();
 
     if current_time < token.claims.exp {
         Ok(())
     } else {
-        Err(Error::TokenExpired)
+        Err(ErrorInJwt::TokenExpired)
     }
 }
 
 // JWT auxiliar functions
-pub fn get_issuer(token: &TokenData<Claims>) -> Result<String, Error> {
+pub fn get_issuer(token: &TokenData<Claims>) -> Result<String, ErrorInJwt> {
     let res = if token.claims.iss.is_empty() {
-        Err(Error::NoIssuer)
+        Err(ErrorInJwt::NoIssuer)
     } else {
         Ok(token.claims.iss.clone())
     };
     res
 }
 
-pub fn get_sub(token: &TokenData<Claims>) -> Result<String, Error> {
+pub fn get_sub(token: &TokenData<Claims>) -> Result<String, ErrorInJwt> {
     let res = if token.claims.sub.is_empty() {
-        Err(Error::NoSub)
+        Err(ErrorInJwt::NoSub)
     } else {
         Ok(token.claims.sub.clone())
     };
@@ -107,7 +110,7 @@ pub fn get_sub(token: &TokenData<Claims>) -> Result<String, Error> {
 // Not sure if needed
 // pub fn get_signature(token: &TokenData<Claims>) -> Result<String, Error> {
 //     let res = if token. {
-//         Err(Error::NoSub)
+//         Err(ErrorInJwt::NoSub)
 //     } else {
 //         Ok(token.claims.sub.clone())
 //     };
@@ -115,30 +118,60 @@ pub fn get_sub(token: &TokenData<Claims>) -> Result<String, Error> {
 // }
 
 // JWKs|JWK auxiliar functions
-pub fn get_jwk(jwt_kid: &String, jwks: &JwkSet) -> Result<Jwk, Error> {
+pub fn get_jwk(jwt_kid: &String, jwks: &JwkSet) -> Result<Jwk, ErrorInJwt> {
     let jwk = jwks
         .keys
         .iter()
         .find(|jwk| jwk.common.key_id.as_ref().map_or(false, |id| id == jwt_kid));
     let res = match jwk {
         Some(json_web_key) => Ok(json_web_key.clone()),
-        None => Err(Error::NoJwkForKid),
+        None => Err(ErrorInJwt::NoJwkForKid),
     };
     res
 }
 
-pub fn verify_jwt(token: TokenData<Claims>, jwks: JwkSet) -> Result<bool, Error> {
-    // Get JWT info
-    check_token(&token)?;
-    let jwt_kid = get_kid_from_token(&token)?;
-    has_token_expired(&token)?;
-    let issuer = get_issuer(&token)?;
-    let subs = get_sub(&token)?;
+pub fn get_signature(token: &str) -> Result<String, ErrorInJwt> {
+    let signature = token.split('.').nth(2);
+    match signature {
+        Some(signature) => Ok(signature.into()),
+        _ => Err(ErrorInJwt::NoSignaturePresent),
+    }
+}
 
-    // Get JWK info
+pub fn get_message(token: &str) -> Result<String, ErrorInJwt> {
+    let message = token.split('.').nth(1);
+    match message {
+        Some(message) => Ok(message.into()),
+        _ => Err(ErrorInJwt::NoSignaturePresent),
+    }
+}
+
+pub fn verify_jwt(token: &str, jwks: JwkSet) -> Result<bool, ErrorInJwt> {
+    let token_header = decode_header(token).unwrap(); // ToDo Handle error
+    let jwt_kid = get_kid_from_token(&token_header)?;
     let jwk = get_jwk(&jwt_kid, &jwks)?;
-    let decode_key = get_public_key(&jwt_kid, &jwk)?;
-    // let res = verify(signature, message, &decode_key, jsonwebtoken::Algorithm::RS256)?;
+    let decode_key = get_public_key(&jwk)?;
+    let token_data =
+        decode::<Claims>(token, &decode_key, &Validation::new(Algorithm::RS256)).unwrap(); // ToDo Handle error
+    // Get JWT info
+    check_token(&token_data)?;
 
-    todo!()
+    has_token_expired(&token_data)?;
+    let _issuer = get_issuer(&token_data)?;
+    let _subs = get_sub(&token_data)?;
+
+    // Extract signature
+    let signature = get_signature(&token)?;
+    let message = get_message(&token)?;
+    // Get JWK info
+    let res = verify(
+        &signature,
+        message.as_bytes(),
+        &decode_key,
+        jsonwebtoken::Algorithm::RS256,
+    );
+    match res {
+        Ok(x) => Ok(x),
+        _ => Err(ErrorInJwt::ErrorVerifying),
+    }
 }
