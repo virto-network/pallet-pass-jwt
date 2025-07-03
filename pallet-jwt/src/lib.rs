039dba140e3b67f2663f4b089ceb5f02486841a7
Lex Luthor <johan@nulo.network>
2025-07-03 23:58:59 +0200

    Fixed comparison, it should compare with interval value and not with zero

--- pallet-jwt/src/lib.rs
+++ pallet-jwt/src/lib.rs
@@ -763,7 +763,7 @@ pub mod pallet {
                 if let Some(interval) = issuer.interval_update {
                     CounterIntervalUpdateIssuer::<T>::mutate(&domain, |counter| {
                         *counter = (*counter + 1) % interval; // Increase counter by one
-                        if 0 == *counter && issuer.is_enabled {
+                        if interval == *counter && issuer.is_enabled {
                             // Only proceed if counter was zero (i.e., time to process)
                             // Check if DomainAccsVec count >= MinimalConsensusPercentage
                             let min_consensus = T::MinimalConsensusPercentage::get();
